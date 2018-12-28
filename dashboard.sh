364ab61a1f78e6db02f711f0df4b73862db06492
iliagerman <iliagerman@gmail.com>
2018-12-29 01:20:36 +0200

    updated script

--- dashboard.sh
+++ dashboard.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+export AWS_PROFILE="sela"
+pulumi stack output kubeconfig >kubeconfig.json
+cp kubeconfig.json /Users/iliagerman/.kube/pulumi_config
 cluster="$(KUBECONFIG=./kubeconfig.json kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')"
 token="$(KUBECONFIG=./kubeconfig.json kubectl -n kube-system describe secret ${cluster}) | grep token"
-KUBECONFIG=./kubeconfig.json kubectl proxy
 echo "token: ${token}"
+KUBECONFIG=./kubeconfig.json kubectl proxy
