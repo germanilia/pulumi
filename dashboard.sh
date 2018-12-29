f68e3deade374673e8c20a35a1124519ad3156d4
iliagerman <iliagerman@gmail.com>
2018-12-29 02:13:09 +0200

    fixed scripts

--- dashboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export AWS_PROFILE="sela"
-kubectl config use-context aws
-cluster="$(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')"
-token="$(kubectl -n kube-system describe secret ${cluster})"
-echo "token: ${token}"
-KUBECONFIG=./kubeconfig.json kubectl proxy
